09b08d2a03113391a467429104e8984ba0ac0c75
Sharon Kim <shakim@chapman.edu>
2017-11-14 20:47:42 +0000

    add comments

--- src/stack/stack.c
+++ src/stack/stack.c
@@ -2,11 +2,16 @@
 #include <stdlib.h>
 #include "stack.h"
 
+//define new_stack function to create a new stack
 STACK new_stack() {
     STACK new = NULL;
     return new;
 }
 
+// create stack to add to the stack
+// use malloc to allocate requested memory and return pointer
+// add data to the new stack
+// assign pointer to the head of the new stack
 void push_stack(STACK *s, SVALUE val) {
     STACK new = (STACK) malloc(sizeof(struct stack));
     if (new == NULL) {
@@ -18,6 +23,13 @@ void push_stack(STACK *s, SVALUE val) {
     *s = new;
 }
 
+// Create pop_stack function
+// check if the stack is empty; have it print error if == NULL
+// Assign val variable to equal the first component in stack data
+// Store the stack data to the variable temp
+// Assign pointer to equal the head of the pointer of the stack
+// use the free function to dealocate memory
+// return current stack data
 SVALUE pop_stack(STACK *s) {
     SVALUE val;
     STACK temp;
@@ -35,7 +47,7 @@ SVALUE pop_stack(STACK *s) {
     return val;
 }
 
-
+// Function to print svalue
 void print_svalue(SVALUE data){
     // Printer for svalue
     printf("%d\n", data);
